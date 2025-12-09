5b6fde2f1456d6d335a1110c6570781c01bfb57c
Глеб Усольцев <glebus008@gmail.com>
2025-12-09 18:43:17 +0500

    Task0

--- Tyuiu.UsoltsevGP.Sprint6.Task0.V18/Form1.Designer.cs
+++ Tyuiu.UsoltsevGP.Sprint6.Task0.V18/Form1.Designer.cs
@@ -28,12 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(29, 32);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(520, 116);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Вычислите выражение по формуле";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(29, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Условие";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(392, 56);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(121, 64);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(29, 173);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(303, 116);
+            textBox2.TabIndex = 3;
+            textBox2.Text = "Ввод данных";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(338, 173);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(211, 116);
+            textBox3.TabIndex = 4;
+            textBox3.Text = "Вывод данных";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(48, 212);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Переменная X:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(48, 230);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 6;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(348, 212);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Результат:";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(348, 230);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.ReadOnly = true;
+            textBox5.Size = new Size(187, 46);
+            textBox5.TabIndex = 8;
+            textBox5.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(348, 304);
+            button1.Name = "button1";
+            button1.Size = new Size(50, 36);
+            button1.TabIndex = 9;
+            button1.Text = "?";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(404, 304);
+            button2.Name = "button2";
+            button2.Size = new Size(145, 36);
+            button2.TabIndex = 10;
+            button2.Text = "Выполнить";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(582, 352);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(label3);
+            Controls.Add(textBox4);
+            Controls.Add(label2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Спринт 6 | Таск 0 | Вариант 18 | Усольцев Г. П.";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label2;
+        private TextBox textBox4;
+        private Label label3;
+        private TextBox textBox5;
+        private Button button1;
+        private Button button2;
     }
 }
--- Tyuiu.UsoltsevGP.Sprint6.Task0.V18/Form1.cs
+++ Tyuiu.UsoltsevGP.Sprint6.Task0.V18/Form1.cs
@@ -1,3 +1,4 @@
+﻿using Tyuiu.UsoltsevGP.Sprint6.Task0.V18.Lib;
 namespace Tyuiu.UsoltsevGP.Sprint6.Task0.V18
 {
     public partial class Form1 : Form
@@ -5,6 +6,62 @@ namespace Tyuiu.UsoltsevGP.Sprint6.Task0.V18
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Получаем значение X из textBox4 (пустое поле рядом с "Переменная X:")
+                int x = int.Parse(textBox4.Text);
+
+                // Проверка деления на ноль
+                if (x == 0)
+                {
+                    MessageBox.Show("Ошибка: X не может быть 0 (деление на ноль)!",
+                                    "Ошибка",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // Вычисляем по формуле y(x) = (x³ - 8) / (2x²)
+                DataService ds = new DataService();
+                double result = ds.Calculate(x);
+
+                // Выводим результат в textBox5 (пустое поле под "Результат:")
+                textBox5.Text = result.ToString("F3"); // 3 знака после запятой
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ошибка ввода! Введите целое число в поле X.",
+                                "Ошибка",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}",
+                                "Ошибка",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Кнопка с вопросом (очистка)
+            // Очищаем поле ввода и результат
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox4.Focus(); // Фокус на поле ввода X
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
