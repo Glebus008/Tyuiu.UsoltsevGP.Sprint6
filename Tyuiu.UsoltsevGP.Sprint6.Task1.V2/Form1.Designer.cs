ed0e844ddc7d4ef58b0316f0045dda3bc00aaf1d
Глеб Усольцев <glebus008@gmail.com>
2025-12-09 19:43:30 +0500

    Task1

--- Tyuiu.UsoltsevGP.Sprint6.Task1.V2/Form1.Designer.cs
+++ Tyuiu.UsoltsevGP.Sprint6.Task1.V2/Form1.Designer.cs
@@ -28,12 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label4 = new Label();
+            textBox6 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(35, 53);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(432, 180);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Протабулировать функцию F(x) на заданном диапазоне. Результат вывести в виде таблицы.";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(35, 250);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(245, 91);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "Ввод данных";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(484, 53);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(287, 288);
+            textBox3.TabIndex = 2;
+            textBox3.Text = "Вывод данных";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(35, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Условие";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(50, 288);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Старт шага";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(160, 288);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Конец шага";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(50, 306);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(160, 306);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Highlight;
+            button1.Location = new Point(286, 250);
+            button1.Name = "button1";
+            button1.Size = new Size(69, 91);
+            button1.TabIndex = 8;
+            button1.Text = "Справка";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.SeaGreen;
+            button2.Location = new Point(361, 250);
+            button2.Name = "button2";
+            button2.Size = new Size(106, 91);
+            button2.TabIndex = 9;
+            button2.Text = "Выполнить";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(497, 78);
+            label4.Name = "label4";
+            label4.Size = new Size(63, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Результат:";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(497, 96);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.ReadOnly = true;
+            textBox6.Size = new Size(262, 233);
+            textBox6.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 373);
+            Controls.Add(textBox6);
+            Controls.Add(label4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button button1;
+        private Button button2;
+        private Label label4;
+        private TextBox textBox6;
     }
 }
--- Tyuiu.UsoltsevGP.Sprint6.Task1.V2/Form1.cs
+++ Tyuiu.UsoltsevGP.Sprint6.Task1.V2/Form1.cs
@@ -1,3 +1,4 @@
+using Tyuiu.UsoltsevGP.Sprint6.Task1.V2.Lib;
 namespace Tyuiu.UsoltsevGP.Sprint6.Task1.V2
 {
     public partial class Form1 : Form
@@ -5,6 +6,59 @@ namespace Tyuiu.UsoltsevGP.Sprint6.Task1.V2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int startStep = int.Parse(textBox4.Text);
+                int stopStep = int.Parse(textBox5.Text);
+
+                if (startStep > stopStep)
+                {
+                    textBox6.Text = "Ошибка: Стартовый шаг не может быть больше конечного!";
+                    return;
+                }
+
+                DataService ds = new DataService();
+                double[] valueArray = ds.GetMassFunction(startStep, stopStep);
+
+                // Очищаем поле
+                textBox6.Text = "";
+
+                // Заголовок таблицы
+                textBox6.AppendText("+----------+----------+" + Environment.NewLine);
+                textBox6.AppendText("|    X     |   f(x)   |" + Environment.NewLine);
+                textBox6.AppendText("+----------+----------+" + Environment.NewLine);
+
+                // Вывод данных
+                for (int i = 0; i < valueArray.Length; i++)
+                {
+                    int x = startStep + i;
+                    // Используем запятую как десятичный разделитель (русский формат)
+                    string formattedValue = valueArray[i].ToString("F2").Replace('.', ',');
+
+                    // Форматирование с фиксированной шириной
+                    string line = $"|{x,6}    |{formattedValue,9} |";
+                    textBox6.AppendText(line + Environment.NewLine);
+                }
+
+                textBox6.AppendText("+----------+----------+" + Environment.NewLine);
+            }
+            catch (FormatException)
+            {
+                textBox6.Text = "Ошибка: Введите целые числа!";
+            }
+            catch (Exception ex)
+            {
+                textBox6.Text = $"Ошибка: {ex.Message}";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = "Введите начало и конец диапазона, затем нажмите 'Выполнить'";
         }
     }
 }
