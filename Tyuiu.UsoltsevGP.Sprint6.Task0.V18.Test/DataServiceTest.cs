4cef6438801046f5cf215621649de2f8b871e25d
Глеб Усольцев <glebus008@gmail.com>
2025-12-09 18:04:15 +0500

    Add project files.

---
+++ Tyuiu.UsoltsevGP.Sprint6.Task0.V18.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.UsoltsevGP.Sprint6.Task0.V18.Lib;
+namespace Tyuiu.UsoltsevGP.Sprint6.Task0.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(3);
+            double wait = 1.056;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
