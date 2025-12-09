ac7a7ac0feec2f825b82c7a9704778f8e430b854
Глеб Усольцев <glebus008@gmail.com>
2025-12-09 19:03:43 +0500

    Task1

---
+++ Tyuiu.UsoltsevGP.Sprint6.Task1.V2/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.UsoltsevGP.Sprint6.Task1.V2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
